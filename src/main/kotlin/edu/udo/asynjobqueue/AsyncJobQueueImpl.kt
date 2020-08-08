e3a383238571d649e3c4ad7c3a3cef5349fb05fc
Björn Lange <blange@itemis.com>
2020-08-08 10:25:50 +0100

    submit does not allow concurrent running jobs

--- src/main/kotlin/edu/udo/asynjobqueue/AsyncJobQueueImpl.kt
+++ src/main/kotlin/edu/udo/asynjobqueue/AsyncJobQueueImpl.kt
@@ -1,9 +1,14 @@
 package edu.udo.asynjobqueue
 
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.Future
 
 internal class AsyncJobQueueImpl(private val executor: ExecutorService) : AsyncJobQueue {
+    private var future: Future<*>? = null
+
     override fun submit(job: Runnable) {
-        executor.submit(job)
+        if (future == null) {
+            future = executor.submit(job)
+        }
     }
 }
