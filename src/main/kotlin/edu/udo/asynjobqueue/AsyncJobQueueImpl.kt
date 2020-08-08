1d251f8e330970608b630c5c5fa6ced498e0d6ef
Björn Lange <blange@itemis.com>
2020-08-08 10:16:38 +0100

    Make executor private

--- src/main/kotlin/edu/udo/asynjobqueue/AsyncJobQueueImpl.kt
+++ src/main/kotlin/edu/udo/asynjobqueue/AsyncJobQueueImpl.kt
@@ -2,7 +2,7 @@ package edu.udo.asynjobqueue
 
 import java.util.concurrent.ExecutorService
 
-internal class AsyncJobQueueImpl(val executor: ExecutorService) : AsyncJobQueue {
+internal class AsyncJobQueueImpl(private val executor: ExecutorService) : AsyncJobQueue {
     override fun submit(job: Runnable) {
         executor.submit(job)
     }
