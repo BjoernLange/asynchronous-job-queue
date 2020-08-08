630f58c914f7b1d0f32030175e3c2d0c43142a68
Björn Lange <blange@itemis.com>
2020-08-08 10:02:15 +0100

    Added documentation to AsyncJobQueue

--- src/main/kotlin/edu/udo/asynjobqueue/AsyncJobQueue.kt
+++ src/main/kotlin/edu/udo/asynjobqueue/AsyncJobQueue.kt
@@ -2,6 +2,15 @@ package edu.udo.asynjobqueue
 
 import java.util.concurrent.ExecutorService
 
+/**
+ * The {@link AsyncJobQueue} implements a FIFO queue of jobs which are run on
+ * an {@link ExecutorService} provided during initialization. It is ensured
+ * that at most one job runs at a time thus enforcing a sequential nature on
+ * the queued jobs.
+ *
+ * In case no job is queued no resources are consumed because no polling or
+ * blocking get operations are used for implementation.
+ */
 interface AsyncJobQueue {
     companion object {
         fun create(executor: ExecutorService): AsyncJobQueue = AsyncJobQueueImpl()
