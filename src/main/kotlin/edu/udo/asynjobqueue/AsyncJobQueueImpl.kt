c92a7321faf7e88841f51d336b5984f7aa093d0f
Björn Lange <blange@itemis.com>
2020-08-08 11:40:13 +0100

    Schedule the next runnable

--- src/main/kotlin/edu/udo/asynjobqueue/AsyncJobQueueImpl.kt
+++ src/main/kotlin/edu/udo/asynjobqueue/AsyncJobQueueImpl.kt
@@ -5,10 +5,19 @@ import java.util.concurrent.Future
 
 internal class AsyncJobQueueImpl(private val executor: ExecutorService) : AsyncJobQueue {
     private var future: Future<*>? = null
+    private var next: Runnable? = null
 
     override fun submit(job: Runnable) {
         if (future?.isDone != false) {
-            future = executor.submit(job)
+            future = executor.submit(Runnable {
+                job.run()
+                val nr = next
+                if (nr != null) {
+                    executor.submit(nr)
+                }
+            })
+        } else {
+            next = job
         }
     }
 }
