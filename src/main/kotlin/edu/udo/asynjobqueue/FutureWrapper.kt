e1bb3bed368a5f6751eb7765142dcf0c1cbe33bf
Björn Lange <blange@itemis.com>
2020-08-30 16:07:35 +0100

    FutureWrapper implements isCancelled

--- src/main/kotlin/edu/udo/asynjobqueue/FutureWrapper.kt
+++ src/main/kotlin/edu/udo/asynjobqueue/FutureWrapper.kt
@@ -27,6 +27,6 @@ internal class FutureWrapper(var wrapped: Future<Future<*>>) : Future<Any?> {
     }
 
     override fun isCancelled(): Boolean {
-        TODO("Not yet implemented")
+        return false
     }
 }
